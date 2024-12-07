7db93f320e27514099507cbaf000060b31adc155
devkavin <kavindra.senanayake@gmail.com>
2024-12-07 21:15:14 +0530

    feat: Add controllers and models for Concession, Kitchen, and Order resources

---
+++ app/Http/Controllers/ConcessionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Concession;
+use Illuminate\Http\Request;
+
+class ConcessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Concession $concession)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Concession $concession)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Concession $concession)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Concession $concession)
+    {
+        //
+    }
+}
