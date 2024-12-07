3157d462824f58f0e893618be23b699ed56c2097
devkavin <kavindra.senanayake@gmail.com>
2024-12-07 21:16:53 +0530

    chore: Add StaffController with CRUD methods for staff management

--- database/migrations/2024_12_07_154433_add_status_to_users_table.php
+++ database/migrations/2024_12_07_154433_add_status_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('status')->default(1); // 1 = Active, 0 = Inactive
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 };
