5c6434276fe953f14b84720f30bb88facd640450
devkavin <kavindra.senanayake@gmail.com>
2024-12-07 21:49:17 +0530

    chore: Add routes for StaffController and update profile routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ConcessionController;
+use App\Http\Controllers\KitchenController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,14 +15,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::group(['middleware' => ['role:Admin']], function () {
     Route::resource('concessions', ConcessionController::class);
+    Route::resource('staff', StaffController::class);
 });
 
 Route::group(['middleware' => ['role:Staff|Admin']], function () {
