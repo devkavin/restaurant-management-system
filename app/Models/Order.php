b0a4ef1174708b0fdb083785408c539f356f4e0e
devkavin <kavindra.senanayake@gmail.com>
2024-12-11 07:04:52 +0530

    feat: Add HasFactory trait and relationships to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'send_to_kitchen_time', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function concessions()
+    {
+        return $this->belongsToMany(Concession::class, 'order_concession')
+            ->withPivot('quantity');
+    }
 }
