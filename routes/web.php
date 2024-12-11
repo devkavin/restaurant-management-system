eee609c7ee21064df4f7543c7da8e3558b8cde7b
devkavin <kavindra.senanayake@gmail.com>
2024-12-11 12:46:29 +0530

    Update web.php to redirect to kitchen dashboard for staff and admin users

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    // redirect to kitchen dashboard
+    if (auth()->user()->hasRole('Staff|Admin')) {
+        return redirect()->route('kitchen.dashboard');
+    }
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -34,6 +37,7 @@ Route::group(['middleware' => ['role:Staff|Admin']], function () {
     Route::post('orders/{order}/send-to-kitchen', [OrderController::class, 'sendToKitchen'])->name('orders.send-to-kitchen');
 
     Route::get('/kitchen', [KitchenController::class, 'index'])->name('kitchen.index');
+    Route::get('/kitchen/dashboard', [KitchenController::class, 'dashboard'])->name('kitchen.dashboard');
     Route::get('/kitchen/orders', [KitchenController::class, 'index'])->name('kitchen.orders.index');
     Route::patch('/kitchen/orders/{order}', [KitchenController::class, 'update'])->name('kitchen.orders.update');
 });
